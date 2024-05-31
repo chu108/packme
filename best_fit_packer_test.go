989ab42eadc93706ac4c45b7c5430fc5fc889919
chu108 <879656448@qq.com>
2024-05-31 14:12:09 +0800

    item update

--- best_fit_packer_test.go
+++ best_fit_packer_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/stevenferrer/packme"
+	"github.com/chu108/packme"
 )
 
 func TestBestFitPacker(t *testing.T) {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/stevenferrer/packme
+module github.com/chu108/packme
 
 go 1.16
 
